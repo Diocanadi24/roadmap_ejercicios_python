8ee807b17ea26efcab85882ebc081ce27a4d0318
Diocanadi <diocanadi2023.cl@gmail.com>
2025-03-17 23:47:51 -0300

    Add files via upload

---
+++ 00_Sintaxis-Variable-Tipos-hola.py
@@ -0,0 +1,27 @@
+# Estamos empezando a aprender Python desde cero y esta es su web: https://www.python.org/
+
+"""
+Esta es la sintaxis de un comentario 
+en varias líneas
+en el lenguaje
+de programación Python
+"""
+my_variable = "Hola Python" # creamos una variable
+
+MY_CONSTANTE = "Adios Python" # creamos una variable en mayúscula para indicar que es una constante y no se debe cambiar su valor. 
+
+name = "Diego"
+print(type(name)) # Tipo de dato 'str'
+
+edad = 36
+print(type(edad)) # Tipo de dato 'int'
+
+estatura = 1.77
+print(type(estatura)) # Tipo de dato 'float'
+
+hombre = True
+print(type(hombre)) # Tipo de dato 'bool'
+
+print("¡Hola, Python!") # la función print sirve para mostrar. 
+
+# Fin del ejercicio.
